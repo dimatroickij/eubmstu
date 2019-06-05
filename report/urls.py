44d1f0f6017329a5d54cd0a4afe65c14f94c9635
Дмитрий Троицкий <dimatroickij@yandex.ru>
2019-06-06 00:28:48 +0300

    Добавлена страница с отчётом

--- report/urls.py
+++ report/urls.py
@@ -4,5 +4,6 @@ from report import views
 
 app_name = 'report'
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('show/<int:code>', views.show, name='show')
 ]
--- report/views.py
+++ report/views.py
@@ -2,7 +2,27 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
+from control.models import Progress, Group, Subject
+
+
 @login_required
 def home(request):
     report = range(1, 7)
-    return render(request,'report/home.html', {'report': report})
+    return render(request, 'report/home.html', {'report': report})
+
+
+def show(request, code):
+    students = Group.objects.get(pk=28519).students.all()
+    reports = Progress.objects.filter(student__in=students)
+    subjects = sorted(list(map(lambda x: {'subject': Subject.objects.get(pk=x['subject']).name,
+                                          'subDep': Subject.objects.get(pk=x['subject']).subdepartament},
+                               reports.filter(student=students[0]).values('subject'))), key=lambda x: x['subject'])
+    mass = []
+    for student in students:
+        mass.append({'student': student, 'progress': sorted(
+            list(map(lambda x: {'subject': x.subject.subject.name, 'point': x.point}, reports.filter(student=student))),
+            key=lambda x: x['subject'])})
+    return render(request, 'report/show.html', {'group': Group.objects.get(pk=28519).name,
+                                                'reports': sorted(mass, key=lambda x: x['student'].last_name),
+                                                'subjects': subjects,
+                                                'semester': Group.objects.get(pk=28519).semester.name})
