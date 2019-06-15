e4e23fa33899da346f5e3318b046f7ca511e1fd9
Дмитрий Троицкий <dimatroickij@yandex.ru>
2019-06-15 18:27:29 +0300

    Исправление внешнего вида

--- report/views.py
+++ report/views.py
@@ -15,26 +15,29 @@ def home(request):
 def show(request, group, code):
     group = Group.objects.get(pk=group)
     students = group.students.all()
-    if code == 1:
-        reports = Progress.objects.filter(student__in=students, subject__group=group)
-    else:
-        reports = Session.objects.filter(student__in=students, subject__group=group)
-    subjects = sorted(list(map(lambda x: {'subject': GroupSubject.objects.get(pk=x['subject']).subject.name,
-                                          'subDep': GroupSubject.objects.get(pk=x['subject']).subject.subdepartament},
-                               reports.filter(student=students[0]).values('subject'))), key=lambda x: x['subject'])
     mass = []
-    if code == 1:
-        for student in students:
-            mass.append({'student': student, 'progress': sorted(
-                list(map(lambda x: {'subject': x.subject.subject.name, 'point': x.point},
-                         reports.filter(student=student))),
-                key=lambda x: x['subject'])})
+    if len(students) != 0:
+        if code == 1:
+            reports = Progress.objects.filter(student__in=students, subject__group=group)
+        else:
+            reports = Session.objects.filter(student__in=students, subject__group=group)
+        subjects = sorted(list(map(lambda x: {'subject': GroupSubject.objects.get(pk=x['subject']).subject.name,
+                                              'subDep': GroupSubject.objects.get(pk=x['subject']).subject.subdepartament},
+                                   reports.filter(student=students[0]).values('subject'))), key=lambda x: x['subject'])
+        if code == 1:
+            for student in students:
+                mass.append({'student': student, 'progress': sorted(
+                    list(map(lambda x: {'subject': x.subject.subject.name, 'point': x.point},
+                             reports.filter(student=student))),
+                    key=lambda x: x['subject'])})
+        else:
+            for student in students:
+                mass.append({'student': student, 'progress': sorted(
+                    list(map(lambda x: {'subject': x.subject.subject.name, 'point': x.rating},
+                             reports.filter(student=student))),
+                    key=lambda x: x['subject'])})
     else:
-        for student in students:
-            mass.append({'student': student, 'progress': sorted(
-                list(map(lambda x: {'subject': x.subject.subject.name, 'point': x.rating},
-                         reports.filter(student=student))),
-                key=lambda x: x['subject'])})
+        subjects = []
     return render(request, 'report/show.html', {'group': group.name,
                                                 'reports': sorted(mass, key=lambda x: x['student'].last_name),
                                                 'subjects': subjects,
