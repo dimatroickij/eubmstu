8bce2dc18741d7ad9adf62524dec7e0a7724a44c
Дмитрий Троицкий <DAlTroitskiy@sberbank.ru>
2023-06-30 00:16:50 +0300

    убрал rabbit из docker compose из-за использования внешнего брокера

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,5 @@ ssh -o StrictHostKeyChecking=no server@$IP_ADDRESS << 'ENDSSH'
   export $(cat .env | xargs)
   docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
   docker pull $IMAGE:eubmstu
-  docker pull library/rabbitmq:3.8-alpine
   docker compose -f docker-compose.prod.yml up -d
 ENDSSH
